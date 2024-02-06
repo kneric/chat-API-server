2e719db84cb8207ba13336ec788eae2a4514bf84
kneric <brian.pradipta.a@gmail.com>
2024-02-06 17:01:17 +0700

    feat: init db deps

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GatewayModule } from './gateway/gateway.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GatewayModule],
+  imports: [ConfigModule.forRoot(), GatewayModule],
   controllers: [AppController],
   providers: [AppService],
 })
